0efbbb0c6df48736cc69309907119eb1eb295858
BlaenkDenum <BlaenkDenum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-17 08:03:44 +0000

    Auto update system screw up
    git-svn-id: http://svn.wp-plugins.org/wp-recaptcha/trunk@55157 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-recaptcha.php
+++ wp-recaptcha.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-reCAPTCHA
 Plugin URI: http://www.blaenkdenum.com/wp-recaptcha/
 Description: Integrates reCAPTCHA anti-spam solutions with wordpress
-Version: 2.8.8
+Version: 2.9
 Author: Jorge Peña
 Email: support@recaptcha.net
 Author URI: http://www.blaenkdenum.com
