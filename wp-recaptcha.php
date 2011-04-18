60fe1323e3f66365a29acccc65515e1c8ea02716
Blaenk Denum <jorgepblank@gmail.com>
2011-04-17 18:00:51 -0700

    getting rid of the duplicate error notice

--- wp-recaptcha.php
+++ wp-recaptcha.php
@@ -19,21 +19,4 @@ require_once('mailhide.php');
 $recaptcha = new reCAPTCHA('recaptcha_options');
 $mailhide = new MailHide('mailhide_options');
 
-function create_error_notice($message, $anchor = '') {
-      $options_url = admin_url('options-general.php?page=wp-recaptcha/recaptcha.php') . $anchor;
-      $error_message = sprintf(__($message . ' <a href="%s" title="WP-reCAPTCHA Options">Fix this</a>', 'recaptcha'), $options_url);
-      
-      echo '<div class="error"><p><strong>' . $error_message . '</strong></p></div>';
-}
-
-function unconfigured_notice() {
-   create_error_notice('You enabled reCAPTCHA, however, neither reCAPTCHA nor MailHide is configured.');
-}
-
-if ($recaptcha->keys_missing() && $recaptcha->keys_missing()) {
-   
-   
-   add_action('admin_notices', 'unconfigured_notice');
-}
-
 ?>
